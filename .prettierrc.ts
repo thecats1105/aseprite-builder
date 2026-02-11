ca41928159111ab258551b486e4ff929e2ca55d0
Taejun Park <thecats1105@gmail.com>
2026-02-11 17:18:34 +0900

    chore(prettier): add Prettier config files
    Add .prettierignore and .prettierrc.ts to standardize formatting across the project.

---
+++ .prettierrc.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'prettier'
+
+const config: Config = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+export default config
